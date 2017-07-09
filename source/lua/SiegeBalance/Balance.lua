c3172711babdb266a9859acf5918e88977933f63
Nin <abielins@gmail.com>
2017-07-09 11:56:28 -0400

    Inital version of hydra buff (increase attack speed)

---
+++ source/lua/SiegeBalance/Balance.lua
@@ -0,0 +1,1 @@
+kHydraDamage = 7.5  -- was 15 for an average of 1 shot per second
--- source/lua/SiegeBalance/FileHooks.lua
+++ source/lua/SiegeBalance/FileHooks.lua
@@ -1,3 +1,4 @@
 ModLoader.SetupFileHook( "lua/Balance.lua", "lua/SiegeBalance/Balance.lua", "post" )
 ModLoader.SetupFileHook( "lua/BalanceHealth.lua", "lua/SiegeBalance/BalanceHealth.lua", "post" )
 ModLoader.SetupFileHook( "lua/BalanceMisc.lua", "lua/SiegeBalance/BalanceMisc.lua", "post" )
+ModLoader.SetupFileHook( "lua/Hydra_Server.lua", "lua/SiegeBalance/Hydra_Server.lua", "post" )
