7fc98ab4f69162465a8426dbdb5429f0bda4a844
Nin <abielins@gmail.com>
2017-07-12 22:04:09 -0400

    Remove back Exo fuel changes completely

--- source/lua/SiegeBalance/Exo.lua
+++
@@ -1,9 +0,0 @@
-kNewThrusterDuration = 2.25
-
-function Exo:GetFuel()
-    if self.thrustersActive then
-        return Clamp(self.fuelAtChange - (Shared.GetTime() - self.timeFuelChanged) / kNewThrusterDuration, 0, 1)
-    else
-        return Clamp(self.fuelAtChange + (Shared.GetTime() - self.timeFuelChanged) / kThrustersCooldownTime, 0, 1)
-    end
-end
