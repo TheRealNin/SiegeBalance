9e646c36184149a52bd27425210c50b689827499
Nin <therealnin@users.noreply.github.com>
2017-07-13 18:42:12 -0400

    Make armories regen armor slowly

--- source/lua/SiegeBalance/FileHooks.lua
+++ source/lua/SiegeBalance/FileHooks.lua
@@ -1,3 +1,4 @@
 ModLoader.SetupFileHook( "lua/Balance.lua", "lua/SiegeBalance/Balance.lua", "post" )
 ModLoader.SetupFileHook( "lua/BalanceHealth.lua", "lua/SiegeBalance/BalanceHealth.lua", "post" )
 ModLoader.SetupFileHook( "lua/BalanceMisc.lua", "lua/SiegeBalance/BalanceMisc.lua", "post" )
+ModLoader.SetupFileHook( "lua/Armory_Server.lua", "lua/SiegeBalance/Armory_Server.lua", "post" )
