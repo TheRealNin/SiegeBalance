6570b9ff2da4c3a03b9bcf7c45a4dbb95a0b5b47
Steelcap <Darkblade.1@gmail.com>
2017-07-13 15:14:53 -0700

    Add files via upload

---
+++ source/lua/SiegeBalance/postMAC.lua
@@ -0,0 +1,20 @@
+Script.Load("lua/ResearchMixin.lua")
+Script.Load("lua/RecycleMixin.lua")
+
+
+local oldfunc = MAC.OnCreate
+local networkVars =
+{
+    welding = "boolean",
+    constructing = "boolean",
+    moving = "boolean",
+}
+AddMixinNetworkVars(ResearchMixin, networkVars)
+AddMixinNetworkVars(RecycleMixin, networkVars)
+
+
+function MAC:OnCreate()
+	oldfunc(self)
+	InitMixin(self, ResearchMixin)
+	InitMixin(self, RecycleMixin)
+end
