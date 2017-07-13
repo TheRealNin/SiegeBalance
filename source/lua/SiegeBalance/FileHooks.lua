52fd4c090823a61de8fd9d969e1f99e0ae702140
Nin <therealnin@users.noreply.github.com>
2017-07-12 22:04:09 -0400

    Remove back Exo fuel changes completely

--- source/lua/SiegeBalance/FileHooks.lua
+++ source/lua/SiegeBalance/FileHooks.lua
@@ -1,4 +1,3 @@
 ModLoader.SetupFileHook( "lua/Balance.lua", "lua/SiegeBalance/Balance.lua", "post" )
 ModLoader.SetupFileHook( "lua/BalanceHealth.lua", "lua/SiegeBalance/BalanceHealth.lua", "post" )
 ModLoader.SetupFileHook( "lua/BalanceMisc.lua", "lua/SiegeBalance/BalanceMisc.lua", "post" )
-ModLoader.SetupFileHook( "lua/Exo.lua", "lua/SiegeBalance/Exo.lua", "post")
