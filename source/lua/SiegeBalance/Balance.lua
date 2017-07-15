33d084864defba9fbd7ae72ef66aeb662fd5bac1
Steelcap <Darkblade.1@gmail.com>
2017-07-14 21:58:38 -0700

    Update Balance.lua

--- source/lua/SiegeBalance/Balance.lua
+++ source/lua/SiegeBalance/Balance.lua
@@ -2,8 +2,8 @@
 kBoneShieldDamageReduction = 0.05 -- was 0.2 --80% for frontal, if it's on-screen(default fov) it's reduced
 kBoneShieldSustainCost =  0 -- was 22
 
-kSkulkGestateTime = 2
+kSkulkGestateTime = 3
 kGorgeGestateTime = 4
-kLerkGestateTime = 9
-kFadeGestateTime = 15
-kOnosGestateTime = 18
+kLerkGestateTime = 7
+kFadeGestateTime = 11
+kOnosGestateTime = 13
