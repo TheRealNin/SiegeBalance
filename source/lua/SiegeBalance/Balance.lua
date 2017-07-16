5d0a4a5d95b96dd2988ce71d44065f05c4abb2ed
Nin <therealnin@users.noreply.github.com>
2017-07-15 22:17:01 -0400

    Fix mist gestation still speeding up

--- source/lua/SiegeBalance/Balance.lua
+++ source/lua/SiegeBalance/Balance.lua
@@ -2,8 +2,11 @@
 kBoneShieldDamageReduction = 0.05 -- was 0.2 --80% for frontal, if it's on-screen(default fov) it's reduced
 kBoneShieldSustainCost =  0 -- was 22
 
-kSkulkGestateTime = 3
-kGorgeGestateTime = 4
-kLerkGestateTime = 7
-kFadeGestateTime = 11
-kOnosGestateTime = 13
+kSkulkGestateTime = 3 -- was 3
+kGorgeGestateTime = 4 -- was 7
+kLerkGestateTime = 7  -- was 15
+kFadeGestateTime = 11 -- was 25
+kOnosGestateTime = 13 -- was 30
+
+-- this is actually very buggy, and is 1-(x/100)
+kNutrientMistPercentageIncrease =  -100 -- was 66
--- source/lua/SiegeBalance/BalanceMisc.lua
+++ source/lua/SiegeBalance/BalanceMisc.lua
@@ -1,3 +1,5 @@
 -- Match Hive Infestation radius to Cyst Infestation radius
 -- Makes building siege rooms easier.
 kHiveInfestationRadius = 7.5 -- was 20
+
+kUpgradeGestationTime = 1 -- was 2
