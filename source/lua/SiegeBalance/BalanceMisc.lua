4ea01b881161e6eabf65403b24991bcdbe204097
Nin <therealnin@users.noreply.github.com>
2017-07-17 10:51:58 -0400

    Increase jetpack replenish rate by 25%, reduce fuel use by 25%

--- source/lua/SiegeBalance/BalanceMisc.lua
+++ source/lua/SiegeBalance/BalanceMisc.lua
@@ -1,3 +1,6 @@
 -- Match Hive Infestation radius to Cyst Infestation radius
 -- Makes building siege rooms easier.
 kHiveInfestationRadius = 7.5 -- was 20
+
+kJetpackUseFuelRate = .21 * 1.25 -- was .21
+kJetpackReplenishFuelRate = .11 * 1.25 -- was .11
