e85f52b2f2ab13b7afa4849a1bfd248ac7b3e58a
Nin <therealnin@users.noreply.github.com>
2017-07-22 17:13:13 -0400

    Add 25% hp for all alien lifeforms

--- source/lua/SiegeBalance/BalanceHealth.lua
+++ source/lua/SiegeBalance/BalanceHealth.lua
@@ -1,5 +1,25 @@
 
 kOnosHealth = 900 * 1.5 -- was 900    
 kOnosArmor = 450  * 1.5-- was 450   
+kOnosArmorFullyUpgradedAmount = 650* 1.5 -- was 650 
 kOnosPointValue = 30    
 kOnosHealtPerBioMass = 30 * 1.5 -- was 30
+
+
+
+local mult = 1.25 
+
+kSkulkHealth = 75*mult    kSkulkArmor = 10*mult   kSkulkHealthPerBioMass = 3*mult
+kGorgeHealth = 160*mult   kGorgeArmor = 75*mult   kGorgeHealthPerBioMass = 2*mult
+kLerkHealth = 150*mult    kLerkArmor = 45*mult    kLerkHealthPerBioMass = 2*mult
+kFadeHealth = 250*mult    kFadeArmor = 80*mult    kFadeHealthPerBioMass = 5*mult
+--kOnosHealth = 900    kOnosArmor = 450   kOnosHealtPerBioMass = 30
+kEggHealth = 350*mult  
+kBabblerHealth = 10*mult  
+        
+kSkulkArmorFullyUpgradedAmount = 30*mult
+kGorgeArmorFullyUpgradedAmount = 100*mult
+kLerkArmorFullyUpgradedAmount = 60*mult
+kFadeArmorFullyUpgradedAmount = 120*mult
+
+kDrifterHealth = 300*mult    kDrifterArmor = 20*mult
