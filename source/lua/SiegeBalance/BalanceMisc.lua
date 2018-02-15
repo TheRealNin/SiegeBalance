d024cb860f42de4a594bae196de4029400f67220
Nin <therealnin@users.noreply.github.com>
2018-02-15 16:48:55 -0500

    Revert jetpack fuel use rate to vanilla

--- source/lua/SiegeBalance/BalanceMisc.lua
+++ source/lua/SiegeBalance/BalanceMisc.lua
@@ -2,7 +2,7 @@
 -- Makes building siege rooms easier.
 kHiveInfestationRadius = 7.5 -- was 20
 
-kJetpackUseFuelRate = .21 * 0.75 -- was .21
+kJetpackUseFuelRate = .21 * 1.0 -- was .21
 kJetpackReplenishFuelRate = .11 * 1.25 -- was .11
 
 kUpgradeGestationTime = 0.6 -- was 2
