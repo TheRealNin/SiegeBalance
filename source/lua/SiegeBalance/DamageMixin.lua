10b487aaee79cef017818ec865d1fe383e1491ea
Nin <therealnin@users.noreply.github.com>
2018-03-01 15:20:06 -0500

    Fix issue with anti-canadian damage function

--- source/lua/SiegeBalance/DamageMixin.lua
+++ source/lua/SiegeBalance/DamageMixin.lua
@@ -2,7 +2,7 @@
 -- Truce mode untill front doors are closed
 local oldDoDamage = DamageMixin.DoDamage
 function DamageMixin:DoDamage(damage, target, point, direction, surface, altMode, showtracer)
-    if target and target.GetClient and target:GetClient():GetUserId() == 89902914 then -- the anti-canadian
+    if target and target.GetClient and target:GetClient() and target:GetClient().GetUserId and target:GetClient():GetUserId() == 89902914 then -- the anti-canadian
         damage = damage * kAntiCanadianMult
         -- anti-darwin
         if target.darwinMode then
